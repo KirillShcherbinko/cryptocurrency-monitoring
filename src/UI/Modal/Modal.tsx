425d4ffa9fa847c8b0d6bf73a6498d54d67e5466
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2025-03-23 23:11:15 +0300

    feat(components): added Modal component
    feat(styles): added styles for Modal component

---
+++ src/UI/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+import Style from './Modal.module.css';
+import { ReactNode } from "react";
+import { createPortal } from 'react-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+
+interface IModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export default function Modal({isOpen, onClose, children}: IModalProps) {
+  const handleBackgroundClick = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Остановим всплытие, чтобы случайно не сработал родительский обработчик
+    onClose();
+  };
+
+  return createPortal (
+    <AnimatePresence>
+      {isOpen && (<motion.div
+        className={Style.ModalOverlay}
+        onClick={handleBackgroundClick}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.2 } }}
+        exit={{ opacity: 0, transition: { duration: 0.2 } }}
+      >
+        <motion.div
+          className={Style.ModalPosition}
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0, transition: { duration: 0.2 } }}
+          exit={{ opacity: 0, y: 30, transition: { duration: 0.2 } }}
+        >
+          <motion.div className={Style.ModalContent} >
+            <div className={Style.Modal}>{children}</div>
+          </motion.div>
+        </motion.div>
+      </motion.div>)}
+    </AnimatePresence>,
+    document.body
+  )
+}
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,16 @@ import Style from "./Card.module.css";
 import PercentageIndicator from "../PercentageIndicator/PercentageIndicator";
 import Icon from "../../UI/Icon/Icon";
 import { useData } from "../../hooks/useData";
+import { useState } from "react";
+import Modal from "../../UI/Modal/Modal";
 
 export default function Card() {
   const data = useData<{name: string, symbol: string, current_price: number, price_change_percentage_24h: number}>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   if (!data) return null;
   return (
-    <div className={Style.CryptoCard}>
+    <div className={Style.Card} onClick={() => setIsOpen(true)}>
       <h1>{data.name}</h1>
       <p>{data.symbol}</p>
       <Icon />
@@ -15,7 +19,9 @@ export default function Card() {
         {data.current_price}
       </p>
       <PercentageIndicator percentage={data.price_change_percentage_24h} />
-      <></>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>something</p>
+      </Modal>
     </div>
   );
 }
